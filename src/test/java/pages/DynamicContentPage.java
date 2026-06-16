22f8f9b8a82942e340d4161017e55568a5c69314
Vadim <gall.hgof@gmail.com>
2026-06-16 15:25:37 +0400

    Updated dynamic content tests. Deep improvements of test logic. Added correct annotations and comments. Some Cleanup.

--- src/test/java/pages/DynamicContentPage.java
+++ src/test/java/pages/DynamicContentPage.java
@@ -2,7 +2,6 @@ package pages;
 
 import com.codeborne.selenide.*;
 import config.TheInternetHeroKuAppConfiguration;
-import io.qameta.allure.Allure;
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import utils.Attachments;
@@ -18,22 +17,24 @@ public class DynamicContentPage {
     final String HEADER_EXPECTED_TEXT = "Dynamic Content";
     final String ENDPOINT = "/dynamic_content";
     final SelenideElement HEADER = $("h3");
+    private static final String ROW_CONTAINER_CSS = "#content .row";
+    private static final String ROWS_CSS = ".row";
+    private static final String TEXT_CSS = ".large-10";
 
     final SelenideElement withContentStaticButton = $(By.linkText("click here"));
-    final ElementsCollection rows = $("#content .row").$$(".row");
+    final ElementsCollection rows = $(ROW_CONTAINER_CSS).$$(ROWS_CSS);
 
     public DynamicContentPage open() {
         Selenide.open(TheInternetHeroKuAppConfiguration.BASE_URL + ENDPOINT);
         return this;
     }
 
-
     public DynamicContentPage shouldBeValid() {
-        Allure.step("Текст заголовка: " + HEADER_EXPECTED_TEXT, () -> HEADER.shouldHave(text(HEADER_EXPECTED_TEXT)));
-        Allure.step("Есть как минимум 3 элемента в разделе Content", () -> {
+        step("Текст заголовка: " + HEADER_EXPECTED_TEXT, () -> HEADER.shouldHave(text(HEADER_EXPECTED_TEXT)));
+        step("Есть как минимум 3 элемента в разделе Content", () -> {
             Assertions.assertThat(rows.size()).isGreaterThanOrEqualTo(3);
         });
-        Allure.step("У каждого элемента страницы есть картинка и текст", () -> {
+        step("У каждого элемента страницы есть картинка и текст", () -> {
             validateRows();
         });
         return this;
@@ -42,48 +43,23 @@ public class DynamicContentPage {
     // Опасно проверять по умолчанию однажды инициализированную коллекцию ROWS без обновления.
     // Поэтому при каждом вызове метода берем всегда свежие данные.
     public DynamicContentPage validateRows() {
-        validateRows($("#content .row").$$(".row"));
+        validateRows($(ROW_CONTAINER_CSS).$$(ROWS_CSS));
         return this;
     }
 
     public DynamicContentPage validateRows(ElementsCollection rows) {
         for (SelenideElement row : rows) {
-            row.$("img").shouldBe(Condition.visible);
-            Assertions.assertThat(row.$(".large-10").text()).hasSizeGreaterThan(10);
-
-            // DEBUG метод удалить перед ревью
-            printRow(row);
+            row.$("img").shouldBe(visible);
+            Assertions.assertThat(row.$(TEXT_CSS).text()).isNotBlank();
         }
-        return this;
-    }
-
-    public DynamicContentPage onlyOnePageElementShouldBeUpdated() {
-
-        List<String> originalElementsTexts = $("#content .row").$$(".row").texts();
-        step("Запоминаем текст всех элементов", () -> {
-            Attachments.attachTextToAllure("Начальный текст элементов", String.join("\n\n", originalElementsTexts));
-        });
-
-        step("Обновляем страницу", () -> {
-            updateRows();
-        });
-
-        List<String> resultElementsTexts = $("#content .row").$$(".row").texts();
-        List<String> difference = new ArrayList<>(originalElementsTexts);
-        difference.removeAll(resultElementsTexts);
-        step("Проверяем что изменился только один элемент на странице", () -> {
-            Assertions.assertThat(difference.size()).isEqualTo(1);
-        });
-
-        step("Сохраняем финальный текст элементов после рефреша страницы", () -> {
-            Attachments.attachTextToAllure("Конечный текст элементов", String.join("\n\n", resultElementsTexts));
-        });
-        System.out.println(difference);
         return this;
     }
 
     public DynamicContentPage setWithContentStatic() {
         withContentStaticButton.click();
+        step("Проверяем что параметр 'with_content=static' включен", () -> {
+            Assertions.assertThat(WebDriverRunner.driver().url()).contains("?with_content=static");
+        });
         return this;
     }
 
@@ -92,11 +68,113 @@ public class DynamicContentPage {
         return this;
     }
 
-    // DEBUG метод удалить перед ревью
-    private void printRow(SelenideElement row) {
-        System.out.printf("Название изображения: %s\nТекст элемента: %s\n\n"
-                , row.$("img").getAttribute("src")
-                , row.$(".large-10").text()
-        );
+    public DynamicContentPage onlyOnePageElementShouldBeUpdated() {
+        updatePageAndAssertRowDifferenceCountIs(1);
+        return this;
+    }
+
+    public DynamicContentPage allPageElementsUpdatedWithPageReload() {
+        updatePageAndAssertRowDifferenceCountIs($(ROW_CONTAINER_CSS).$$(ROWS_CSS).size());
+        return this;
+    }
+
+
+    private DynamicContentPage updatePageAndAssertRowDifferenceCountIs(int expectedElementsDifferenceCount) {
+        List<String> originalElementsTexts = getListOfRowsTexts();
+        validateArguments(expectedElementsDifferenceCount, originalElementsTexts.size());
+
+        step("Сохраняем текст всех элементов", () -> {
+            Attachments.attachTextToAllure("Начальный текст элементов", String.join("\n\n", originalElementsTexts));
+        });
+
+        //Определяем корректный комментарий к шагу
+        String assertionStepComment = getValidAssertionsCommentWith(expectedElementsDifferenceCount, originalElementsTexts.size());
+
+        step("Обновляем страницу", this::updateRows);
+
+        List<String> resultElementsTexts = $(ROW_CONTAINER_CSS).$$(ROWS_CSS).texts();
+        step("Сохраняем текст всех элементов после обновления страницы", () -> {
+            Attachments.attachTextToAllure("Текст элементов после обновления", String.join("\n\n", resultElementsTexts));
+        });
+
+        List<String> difference = new ArrayList<>(resultElementsTexts);
+        step("Находим разницу между элементами", () -> {
+            difference.removeAll(originalElementsTexts);
+        });
+
+        step(assertionStepComment, () -> {
+            Assertions.assertThat(difference.size()).isEqualTo(expectedElementsDifferenceCount);
+        });
+
+        if (expectedElementsDifferenceCount > 0) {
+            step("Сохраняем итоговую разницу в текстах элементов", () -> {
+                Attachments.attachTextToAllure("Текст элементов после обновления", String.join("\n\n", difference));
+            });
+        }
+        return this;
+    }
+
+    //Получаем список всех текстов на странице по селектору, проверяем только наличие изображений
+    private static List<String> getListOfRowsTexts() {
+        List<String> result = new ArrayList<>(List.of());
+        for (SelenideElement row : $(ROW_CONTAINER_CSS).$$(ROWS_CSS)) {
+            //Проверяем что у каждого ROWS есть изображение и оно отображается на странице
+            row.$("img").shouldBe(visible);
+            result.add(row.text());
+        }
+        return result;
+    }
+
+    //В зависимости от ожидаемого значения количества измененных элементов, подбираем корректный комментарий для отчетности
+    private static String getValidAssertionsCommentWith(int expectedElementsDifferenceCount, int expectedElementsCount) {
+        String assertionStepComment;
+        switch (expectedElementsDifferenceCount) {
+            case (0):
+                assertionStepComment = "Проверяем что текст элементов страницы не изменился";
+                break;
+            case (1): {
+                assertionStepComment = "Проверяем что изменился текст только одного элемента на странице";
+                break;
+            }
+            case (2), (3): {
+                if (expectedElementsDifferenceCount == expectedElementsCount) {
+                    assertionStepComment = "Проверяем что текст всех элементов страницы обновлен.";
+                } else {
+                    assertionStepComment = "Проверяем что изменился текст только " + expectedElementsDifferenceCount + " элементов на странице";
+                }
+                break;
+            }
+            default:
+                assertionStepComment = "Проверяем что изменилось " + expectedElementsDifferenceCount + " элементов.";
+        }
+        return assertionStepComment;
+    }
+
+    private static void validateArguments(int expectedElementsDifferenceCount, int originalElementsTextsSize) {
+        if(originalElementsTextsSize <= 0){
+            throw new IllegalArgumentException(
+                    String.format("""
+                            Count of page rows elements is equal or less 0.
+                            [expectedElementsDifferenceCount]: [%s]
+                            [rows.size()]: [%s]
+                            """, expectedElementsDifferenceCount, originalElementsTextsSize)
+            );
+        }
+        if (expectedElementsDifferenceCount < 0) {
+            throw new IllegalArgumentException(
+                    String.format("""
+                            Expected elements difference count can not be less than 0.
+                            [expectedElementsDifferenceCount]: [%s]
+                            """, expectedElementsDifferenceCount)
+            );
+        }
+        if (expectedElementsDifferenceCount > originalElementsTextsSize) {
+            throw new IllegalArgumentException(
+                    String.format("""
+                            Expected elements difference count can not be more than row elements count.
+                            [expectedElementsDifferenceCount]: [%s]
+                            """, expectedElementsDifferenceCount)
+            );
+        }
     }
 }
--- src/test/java/tests/DynamicContentTests.java
+++ src/test/java/tests/DynamicContentTests.java
@@ -1,19 +1,11 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
-import config.TheInternetHeroKuAppConfiguration;
+import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import pages.DynamicContentPage;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 
 @DisplayName("Проверки раздела 'Dynamic Content'")
@@ -24,17 +16,31 @@ public class DynamicContentTests {
     @Test
     @DisplayName("Проверка элементов страницы")
     void dynamicContentPageShouldBeValid() {
-        step("Открываем 'Dropdown List'", page::open);
+        step("Открываем 'Dynamic Content'", page::open);
         step("Валидируем страницу 'Dropdown List': ", page::shouldBeValid);
     }
 
-    // TODO: реализовать сравнения двух коллекций. Не в лоб по строкам, а прям чтобы показать что изменилась такая-то строка!
-    // Выводить строку!
     @Test
-    @DisplayName("Проверка динамической смены только одного элемента страницы при установке флага")
+    @DisplayName("Проверка динамической смены только одного элемента страницы при установке флага " +
+            "'with_content = static'")
+    @Description("Проверки страницы 'Dynamic Content' ориентируются на изменение текста, а не изображений. " +
+            "Изображение при обновлении страницы может как измениться, так и остаться прежним. " +
+            "Поэтому при каждой проверке проверяется только отображение картинки у тестируемого row элемента.")
     void onlyOnePageElementShouldBeUpdatedWithStaticContentParameter() {
-        step("Открываем 'Dropdown List'", page::open);
+        step("Открываем 'Dynamic Content'", page::open);
         step("Устанавливаем флаг '?with_content=static':", page::setWithContentStatic);
-        step("Проверяем что только один элемент будет обновляться если обновить страницу", page::onlyOnePageElementShouldBeUpdated);
+        step("Проверяем что только один элемент будет обновляться если обновить страницу"
+                , page::onlyOnePageElementShouldBeUpdated);
+    }
+
+    @Test
+    @DisplayName("Проверка динамической смены всех элементов страницы по умолчанию")
+    @Description("Проверки страницы 'Dynamic Content' ориентируются на изменение текста, а не изображений. " +
+            "Изображение при обновлении страницы может как измениться, так и остаться прежним. " +
+            "Поэтому при каждой проверке проверяется только отображение картинки у тестируемого row элемента.")
+    void allPageElementShouldBeUpdatedWithoutStaticContentParameter() {
+        step("Открываем 'Dynamic Content'", page::open);
+        step("Проверяем что все элементы страницы изменяются если не установлен флаг 'with_content = static'"
+                , page::allPageElementsUpdatedWithPageReload);
     }
 }
