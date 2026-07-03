88406299128773004117493d720aafbaf6f8686c
Vadim <gall.hgof@gmail.com>
2026-07-04 01:34:30 +0400

    Added tests for checkbox add/remove. InputFields tests be ready after weekind.

--- src/test/java/pages/DynamicControlsPage.java
+++ src/test/java/pages/DynamicControlsPage.java
@@ -3,19 +3,27 @@ package pages;
 import com.codeborne.selenide.*;
 import config.TheInternetHeroKuAppConfiguration;
 import io.qameta.allure.Step;
-import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
 
 import java.util.Arrays;
 
+import static com.codeborne.selenide.CollectionCondition.*;
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.*;
 
 public class DynamicControlsPage {
     final String ENDPOINT = "/dynamic_controls";
-    final String REMOVED_EXPECTED_TEXT = "It's gone!";
-    final String DISABLED_EXPECTED_TEXT = "It's disabled!";
+    final String CHECKBOX_REMOVED_EXPECTED_TEXT = "It's gone!";
+    final String CHECKBOX_ADDED_EXPECTED_TEXT = "It's back!";
+    final String INPUT_ENABLED_EXPECTED_TEXT = "It's enabled!";
+    final String INPUT_DISABLED_EXPECTED_TEXT = "It's disabled!";
+    final String EXPECTED_CHECKBOX_TEXT = "A checkbox";
+    final String REMOVE_BUTTON_TEXT = "Remove";
+    final String ADD_BUTTON_TEXT = "Add";
+    final String CHECKBOX_FORM_BUTTON_SELECTOR = "#checkbox-example > button";
 
     final String[] EXPECTED_HEADERS_TEXTS = {
             "Dynamic Controls",
@@ -23,17 +31,31 @@ public class DynamicControlsPage {
             "Enable/disable"
     };
 
+    // Выносим в константу селектор ID формы чекбокса для динамического обновления состояния
+    final String CHECKBOX_FORM_SELECTOR = "#checkbox-example";
+    final String INPUT_FORM_SELECTOR = "#input-example";
+
+    final String INPUT_SELECTOR = "input";
+    final String MESSAGE_SELECTOR = "#message";
+    final String LOADER_SELECTOR = "#loading";
+    final String BUTTONS_SELECTOR = "button";
+
+
     final ElementsCollection pageHeaders = $$("#content h4");
 
-    final SelenideElement checkboxInput = $("#checkbox > input");
-    final SelenideElement checkboxText = $("#checkbox > input");
-
-    final SelenideElement inputField = $("#input-example > input");
+    // Находим div у которого точно будет id = checkbox
+    final SelenideElement checkboxElement = $("div#checkbox");
+    final SelenideElement inputField = $("form#input-example > input");
 
     final ElementsCollection buttons = $$("button");
 
-    final SelenideElement removeButton = $("#checkbox-example > button");
-    final SelenideElement enableButton = $("#input-example > button");
+
+    final SelenideElement checkboxFormButton = $(CHECKBOX_FORM_BUTTON_SELECTOR);
+    //final SelenideElement removeCheckboxButton = $(CHECKBOX_FORM_BUTTON_SELECTOR).$(Selectors.byText(REMOVE_BUTTON_TEXT));
+    final SelenideElement addCheckboxButton = $(CHECKBOX_FORM_BUTTON_SELECTOR).$(withText(ADD_BUTTON_TEXT));
+
+
+    final SelenideElement loading = $(LOADER_SELECTOR);
 
     public DynamicControlsPage open() {
         Selenide.open(TheInternetHeroKuAppConfiguration.BASE_URL + ENDPOINT);
@@ -50,22 +72,73 @@ public class DynamicControlsPage {
             }
         });
         step("Чекбокс отображается и доступен", () -> {
-            checkboxInput.shouldBe(enabled, clickable);
-            checkboxText.shouldBe(visible);
+            checkboxElement.$(INPUT_SELECTOR).shouldBe(enabled, clickable);
         });
-        step("Поле ввода отображается и недоступно", () -> {
+        step("Текстовое поле ввода отображается и недоступно", () -> {
             inputField.shouldBe(visible, disabled);
         });
         return this;
     }
 
-    @Step("Переключаем состояние чек-бокса и проверяем изменение")
-    public DynamicControlsPage selectCheckboxAndVerifyState() {
+    @Step("Проверяем что чекбокс отображается и может изменять свое состояние")
+    public DynamicControlsPage verifyCheckBoxStateCanBeChanged() {
         // Динамически инвертируем ожидание, проверяем что значение чек-бокса может быть изменено
-        WebElementCondition expectedCheckBoxState = checkboxInput.isSelected() ? not(selected) : selected;
+        WebElementCondition expectedCheckBoxState = checkboxElement.$(INPUT_SELECTOR).isSelected() ? not(selected) : selected;
 
-        checkboxInput.click();
-        checkboxInput.shouldBe(expectedCheckBoxState);
+        checkboxElement.$(INPUT_SELECTOR).click();
+        checkboxElement.$(INPUT_SELECTOR).shouldBe(expectedCheckBoxState);
+        return this;
+    }
+
+    @Step("Нажимаем клавишу удаления чек-бокса")
+    public DynamicControlsPage clickRemoveCheckboxButton(){
+        $(byText(REMOVE_BUTTON_TEXT)).click();
+        return this;
+    }
+
+    @Step("Нажимаем клавишу добавления чек-бокса")
+    public DynamicControlsPage clickAddCheckboxButton(){
+        $(byText(ADD_BUTTON_TEXT)).click();
+        $(MESSAGE_SELECTOR).shouldHave(text(CHECKBOX_ADDED_EXPECTED_TEXT));
+        $(CHECKBOX_FORM_BUTTON_SELECTOR).shouldHave(text(REMOVE_BUTTON_TEXT));
+        checkboxElement.shouldBe(exist);
+        return this;
+    }
+
+    @Step("Проверяем что лоадер появился на странице в единственном экземпляре")
+    public DynamicControlsPage loaderShouldAppear() {
+        validateThatLoaderFrom(CHECKBOX_FORM_SELECTOR, appear);
+        // Только один элемент лоадера на всю страницу
+        $$(LOADER_SELECTOR).shouldBe(size(1));
+        return this;
+    }
+
+    // После загрузки, лоадер остается в DOM, но становится невидимым (hidden) - приемлемое поведение
+    @Step("Проверяем что лоадер исчез спустя время")
+    public DynamicControlsPage checkboxLoaderShouldDisappear() {
+        validateThatLoaderFrom(CHECKBOX_FORM_SELECTOR, hidden);
+        return this;
+    }
+
+    @Step("Проверяем что чек-бокс отображается на странице")
+    public DynamicControlsPage validateCheckboxAppearAndValid(){
+        checkboxElement.shouldBe(visible, exist);
+        Assertions.assertThat(checkboxElement.text()).isEqualTo(EXPECTED_CHECKBOX_TEXT);
+
+        //Проверяем что чек-бокс в актуальном состоянии и им можно манипулировать
+        verifyCheckBoxStateCanBeChanged();
+        return this;
+    }
+
+    @Step("Проверяем что чек-бокс удален")
+    public DynamicControlsPage validateCheckboxRemovedAfterLoading(){
+        validateButtonTextAndCheckboxStateIs(ADD_BUTTON_TEXT, not(exist));
+        return this;
+    }
+
+    @Step("Проверяем что чек-бокс добавлен на страницу")
+    public DynamicControlsPage validateCheckboxAddedAfterLoading(){
+        validateButtonTextAndCheckboxStateIs(REMOVE_BUTTON_TEXT, appear);
         return this;
     }
 
@@ -73,5 +146,15 @@ public class DynamicControlsPage {
         return Boolean.TRUE.equals(Selenide.executeJavaScript(
                 "return window.performance.getEntriesByType('navigation')[0].type == 'reload';"
         ));
+    }
+
+    private void validateButtonTextAndCheckboxStateIs(String expectedButtonText, WebElementCondition expectedCondition){
+        checkboxLoaderShouldDisappear();
+        checkboxElement.shouldBe(expectedCondition);
+        assertThat($(CHECKBOX_FORM_BUTTON_SELECTOR).text()).isEqualTo(expectedButtonText);
+    }
+
+    private void validateThatLoaderFrom(String formSelector, WebElementCondition expectedCondition){
+        $(formSelector).$(LOADER_SELECTOR).shouldBe(expectedCondition);
     }
 }
--- src/test/java/tests/DynamicControlsTests.java
+++ src/test/java/tests/DynamicControlsTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import io.qameta.allure.Feature;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.DynamicControlsPage;
@@ -21,9 +22,47 @@ public class DynamicControlsTests extends BaseTest{
     }
 
     @Test
-    @DisplayName("Чек-бокс может быть проставлен")
+    @DisplayName("Состояние чек-бокса может быть изменено")
     void checkboxCanBeSelected() {
         step("Открываем 'Dynamic Controls'", page::open);
-        step("Изменяем состояние чек-бокса", page::selectCheckboxAndVerifyState);
+        step("Изменяем состояние чек-бокса", page::verifyCheckBoxStateCanBeChanged);
     }
+
+    @Test
+    @DisplayName("Чек-бокс может быть удален со страницы")
+    void checkboxCanBeRemoved() {
+        step("Открываем 'Dynamic Controls'", page::open);
+        step("Проверяем что с чек-боксом можно взаимодействовать", page::validateCheckboxAppearAndValid);
+        step("Удаляем чек-бокс со страницы", page::clickRemoveCheckboxButton);
+        step("На странице отобразился лоадер загрузки", page::loaderShouldAppear);
+        step("Проверяем что чек-бокс удален", page::validateCheckboxRemovedAfterLoading);
+    }
+
+    @Disabled("Тест - проверка исправления бага с добавлением и удалением элементов checkbox." +
+            "Причина: ID = checkbox присваивается не div-контейнеру, а элементу input т.е. самому чек-боксу. " +
+            "Чисто технически это верно. Однако клавиша Remove удаляет любой элемент формы checkbox-example по " +
+            "id=checkbox. Получается после нажатия на клавишу удаления (ранее добавленного) чек-бокса в DOM остается " +
+            "div с текстом:'A checkbox' что неправильно и засоряет DOM.")
+    @Test
+    @DisplayName("Чек-бокс может быть добавлен на страницу после удаления")
+    void checkboxCanBeAddedAfterRemoving() {
+        step("Открываем 'Dynamic Controls'", page::open);
+        step("Проверяем что с чек-боксом можно взаимодействовать", page::validateCheckboxAppearAndValid);
+        step("Удаляем чек-бокс со страницы", page::clickRemoveCheckboxButton);
+        step("На странице отобразился лоадер загрузки", page::loaderShouldAppear);
+        step("Проверяем что чек-бокс удален", page::validateCheckboxRemovedAfterLoading);
+        step("Нажимаем клавишу добавления чек-бокса", page::clickAddCheckboxButton);
+        step("На странице отобразился лоадер загрузки", page::loaderShouldAppear);
+        step("Проверяем что чек-бокс добавлен", page::validateCheckboxAddedAfterLoading);
+    }
+
+    @Test
+    @DisplayName("Чек-бокс может быть добавлен на страницу после удаления")
+    void checkboxCanBeAddedAfterRemove() {
+        step("Открываем 'Dynamic Controls'", page::open);
+        step("Изменяем состояние чек-бокса", page::verifyCheckBoxStateCanBeChanged);
+    }
+
+
+
 }
